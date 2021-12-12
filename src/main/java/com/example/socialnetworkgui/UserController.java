90f8273ca4bf709f6e24546ca78e10453753deee
Valentina Chioveanu <cvir2893@scs.ubbcluj.ro>
2021-12-13 01:41:46 +0200

    Added Friend Requests View

--- src/main/java/com/example/socialnetworkgui/UserController.java
+++ src/main/java/com/example/socialnetworkgui/UserController.java
@@ -7,15 +7,21 @@ import com.example.socialnetworkgui.service.Service;
 import com.example.socialnetworkgui.service.ServiceException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Locale;
@@ -187,5 +193,36 @@ public class UserController {
             errorLabelText.setTextFill(Paint.valueOf("red"));
             errorLabelText.setText(ime.getMessage());
         }
+    }
+
+    @FXML
+    protected void onFriendRequestsClick() {
+        showFriendRequestsView();
+    }
+
+    public void showFriendRequestsView() {
+        System.out.println(getClass().getResource("views/register-view.fxml"));
+        System.out.println(getClass().getResource("views/friend-requests.fxml"));
+        System.out.println(getClass().getResource("startup-view.fxml"));
+        System.out.println(getClass().getResource("UserView.fxml"));
+        /*try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("views/friend-requests.fxml"));
+
+            AnchorPane root = (AnchorPane) loader.load();
+
+            FriendRequestsController friendRequestsController = loader.getController();
+            friendRequestsController.setService(service);
+            friendRequestsController.setCurrentUser(currentUser);
+
+            Stage friendRequestsStage = new Stage();
+            Scene scene = new Scene(root, 651, 400);
+            friendRequestsStage.setScene(scene);
+            friendRequestsStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+         */
     }
 }
